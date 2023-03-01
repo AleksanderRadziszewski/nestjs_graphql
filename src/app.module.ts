92c50d260de3ae005709cd56cf0274137b151bd3
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2023-03-01 14:19:14 +0100

    docker compose services

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,25 +11,12 @@ import { OwnersModule } from './owners/owners.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot<ApolloDriverConfig>({
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      driver: ApolloDriver,
-    }),
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
-        entities: ['src/entity/**/*.js'],
-        synchronize: true,
-        autoLoadEntities: true,
-      }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
     PetsModule,
     OwnersModule,
